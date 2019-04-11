45742719b189c7bc406f8ece24939f34955553ef
Konno Yo <sphz2729@lagoon.ocn.ne.jp>
2019-04-11 14:50:31 +0000

    PHP02課題

---
+++ PHP02/report3.php
@@ -0,0 +1,11 @@
+<?php
+
+// それぞれの変数に言葉を代入
+$hello = "Hello，";
+$name = "KonnoYo";
+$world = "'s World!";
+
+// 各変数を連結させて表示
+echo $hello,$name,$world,"\n";
+
+?>
---
+++ PHP02/report4.php
@@ -0,0 +1,12 @@
+<?php
+
+// $tech_boostに"tech"という文字列を代入
+$tech_boost = "tech";
+
+// $tech_boostに文字列を連結する
+$tech_boost .= ' boost';
+
+// $tech_boostの文字列を表示
+echo $tech_boost,"\n";
+
+?>
