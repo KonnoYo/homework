00a336cebaf16ed016d3570b12b35c5974e605d9
Konno Yo <sphz2729@lagoon.ocn.ne.jp>
2019-04-12 12:34:49 +0000

    PHP03課題

---
+++ PHP03/report3.php
@@ -0,0 +1,13 @@
+<?php
+
+//$fruitsに配列で好きなフルーツを5個代入
+
+$fruits = array("apple","orange","strawberry","lemon","banana");
+
+//foreach文で順番に出力
+
+foreach ($fruits as $fruit) {
+    echo $fruit,"\n";
+}
+
+?>
