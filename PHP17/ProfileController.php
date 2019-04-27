d0b00e8dc7f9b70e6f31a36b7c802813b91099f3
Konno Yo <sphz2729@lagoon.ocn.ne.jp>
2019-04-27 14:16:16 +0000

    PHP17課題

---
+++ PHP17/ProfileController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+// 以下を追記することでProfile Modelが使えるようになる
+use App\Profile;
+
+// 以下を追記
+use App\History2;
+
+use Carbon\Carbon;
+
+class ProfileController extends Controller
+{
+    public function add()
+    {
+        return view('admin.profile.create');
+    }
+
+    public function create(Request $request)
+    {
+        // Validationを行う
+        $this->validate($request, Profile::$rules);
+
+        $profile = new Profile;
+        $form = $request->all();
+
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        
+        /*
+        // フォームから送信されてきたimageを削除する
+        unset($form['image']);
+        */
+        
+        // データベースに保存する
+        $profile->fill($form);
+        $profile->save();
+        
+        // admin/profile/createにリダイレクトする
+        return redirect('admin/profile/create');
+    }
+
+    public function index(Request $request)
+    {
+        $cond_title = $request->cond_title;
+        if ($cond_title != '') {
+            // 検索されたら検索結果を取得する
+            $posts = Profile::where('name', $cond_title)->get();
+        } else {
+            // それ以外はすべてのプロフィールを取得する
+            $posts = Profile::all();
+        }
+        return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+    }
+
+
+    public function edit(Request $request)
+    {
+        // Profile Modelからデータを取得する
+        $profile = Profile::find($request->id);
+        if (empty($profile)) {
+            abort(404);    
+        }
+        return view('admin.profile.edit', ['profile_form' => $profile]);
+    }
+    
+    
+    public function update(Request $request)
+    {
+        // Validationをかける
+        $this->validate($request, Profile::$rules);
+        // Profile Modelからデータを取得する
+        $profile = Profile::find($request->id);
+        // 送信されてきたフォームデータを格納する
+        $profile_form = $request->all();
+        unset($profile_form['_token']);
+        
+        // 該当するデータを上書きして保存する
+        $profile->fill($profile_form)->save();
+        
+        // Profile Modelを保存するタイミングで、同時に History2 Modelにも編集履歴を追加するよう実装
+        $history2 = new History2;
+        $history2->profile_id = $profile->id;
+        $history2->edited_at = Carbon::now();
+        $history2->save();
+        
+        return redirect('admin/profile');
+    }
+    
+    public function delete(Request $request)
+    {
+        // 該当するProfile Modelを取得
+        $profile = Profile::find($request->id);
+        // 削除する
+        $profile->delete();
+        return redirect('admin/profile/');
+    }  
+    
+}
